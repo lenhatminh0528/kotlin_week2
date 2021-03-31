bbbe7d2833989d314951643a6db94cbb55a1f568
minhlenhat28 <lenhatminh0528@gmail.com>
2021-03-31 15:36:14 +0700

    add switch to verification code screen

--- app/src/main/java/kotlinclass/leminh/kotlin_week2/LoginActivity.kt
+++ app/src/main/java/kotlinclass/leminh/kotlin_week2/LoginActivity.kt
@@ -1,15 +1,21 @@
 package kotlinclass.leminh.kotlin_week2
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btn_login.setOnClickListener{
+            var intent = Intent(this@LoginActivity, VerifyCodeActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
